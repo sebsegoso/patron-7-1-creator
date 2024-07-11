36f5d479f50942d7016914b53b338b32737d3357
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-07-11 11:54:26 -0400

    optimización de script, validación de git y vs code

--- script.sh
+++ script.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-#Limpiar consola
+
+# Limpiar consola
 clear
-# Nombre proyecto
-read -p "Ingrese el nombre del proyecto: " project_name
-project_name=${project_name:-mi-proyectito}
 
-project_name=$(echo $project_name | tr ' ' '-')
+# Función para verificar si un programa está instalado
+function check_command {
+    if ! command -v $1 &> /dev/null; then
+        echo "Error: $1 no está instalado. Por favor instálalo y vuelve a ejecutar el script."
+        exit 1
+    fi
+}
 
-echo "Creando proyecto: " $project_name
-# creando directorio 
-mkdir -p $project_name
-cd $project_name
+# Verificar si Git está instalado
+check_command git
 
-style_file_name="style.css"
+# Verificar si Visual Studio Code está instalado
+check_command code
 
-mkdir assets assets/css assets/js assets/img
+# Función para crear archivos Sass
+function create_sass_structure {
+    mkdir -p assets/sass/{abstracts,base,components,layout,shame,themes,vendors,pages}
+    touch assets/sass/abstracts/_abstracts.scss
+    touch assets/sass/base/_base.scss
+    touch assets/sass/components/_components.scss
+    touch assets/sass/layout/_layout.scss
+    touch assets/sass/shame/_shame.scss
+    touch assets/sass/themes/_themes.scss
+    touch assets/sass/vendors/_vendors.scss
+    touch assets/sass/pages/_pages.scss
 
-# utilizar sass?
-read -p "¿Desea utilizar sass con patrón 7-1? (s/n): " use_sass
-
-if [ "$use_sass" == "s" ]; then
-    style_file_name="main.css"
-
-    mkdir assets/sass
-    cd assets/sass
-    mkdir abstracts base components layout shame themes vendors pages
-    touch abstracts/_abstracts.scss base/_base.scss components/_components.scss layout/_layout.scss shame/_shame.scss themes/_themes.scss vendors/_vendors.scss pages/_pages.scss
-    cd ../..
-
-    cat <<EOL >"assets/sass/main.scss"
+    cat <<EOL > assets/sass/main.scss
 @charset 'UTF-8';
 
 // Abstracts
@@ -37,33 +38,55 @@ if [ "$use_sass" == "s" ]; then
 // Vendors
 @import "vendors/vendors";
 
-// base
+// Base
 @import "base/base";
 
-// layout
+// Layout
 @import "layout/layout";
 
-// components
+// Components
 @import "components/components";
 
-// pages
+// Pages
 @import "pages/pages";
 
-// themes
+// Themes
 @import "themes/themes";
 
-// shame
+// Shame
 @import "shame/shame";
 EOL
-echo "No olvides activar el watcher de Sass"
+    echo "Estructura de Sass creada. No olvides activar el watcher de Sass."
+}
 
+# Nombre del proyecto
+read -p "Ingrese el nombre del proyecto: " project_name
+project_name=${project_name:-my-project}
+
+# Reemplazar espacios por guiones
+project_name=$(echo $project_name | tr ' ' '-')
+
+echo "Creando proyecto: $project_name"
+
+# Crear directorio y navegar a él
+mkdir -p "$project_name"
+cd "$project_name"
+
+# Inicializar estructura de carpetas
+style_file_name="style.css"
+mkdir -p assets/{css,js,img}
+
+# Utilizar Sass
+read -p "¿Desea utilizar Sass con patrón 7-1? (s/n): " use_sass
+if [ "$use_sass" == "s" ]; then
+    style_file_name="main.css"
+    create_sass_structure
 else
     touch assets/css/style.css
 fi
 
-# Crear un archivo HTML con el contenido solicitado
-touch index.html
-cat <<EOL >"index.html"
+# Crear archivo HTML
+cat <<EOL > index.html
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -78,26 +101,22 @@ cat <<EOL >"index.html"
 </html>
 EOL
 
-# Notificar al usuario
 echo "Archivo HTML creado"
 
-# Inicializar git
-read -p "¿Desea inicializar git? (s/n): " git_init
-
+# Inicializar Git
+read -p "¿Desea inicializar Git? (s/n): " git_init
 if [ "$git_init" == "s" ]; then
-    echo "Inicializando git..."
     git init
     touch .gitignore
+    echo "Git inicializado y archivo .gitignore creado."
 else
-    echo "Ok, sin git"
+    echo "Ok, sin Git."
 fi
 
-# Inicializar git
-read -p "¿Desea abrir el proyecto en Visual Studio Code (s/n)" open_vscode
-
+# Abrir proyecto en Visual Studio Code
+read -p "¿Desea abrir el proyecto en Visual Studio Code? (s/n): " open_vscode
 if [ "$open_vscode" == "s" ]; then
     code .
 else
     cd ..
 fi
-
